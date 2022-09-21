d9ce867e76dbeaa31cd85d50310dac8d484d3276
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-09-21 21:22:54 +0100

    Test and code entry form

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,11 +1,20 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('http://localhost:3030scoops', (req, res, ctx) => {
+  rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
     return res(
       ctx.json([
         { name: 'Chocolate', imaPath: '/images/chocolate.png' },
         { name: 'Vanilla', imaPath: '/images/vanilla.png' },
+      ])
+    );
+  }),
+  rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: 'Cherries', imagePath: '/images/cherries.png' },
+        { name: 'M&Ms', imagePath: '/images/m-&-ms.png' },
+        { name: 'Hot Fudge', imagePath: '/images/hot-fudge.png' },
       ])
     );
   }),
